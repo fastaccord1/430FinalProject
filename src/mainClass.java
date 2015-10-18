ed4a0bf701476b65fc997d9bb77896c0564285c7
Kevin Reuter <fastaccord1@gmail.com>
2015-10-18 12:15:38 -0500

    adding class for fresh install

--- src/mainClass.java
+++ src/mainClass.java
@@ -1,6 +1,7 @@
 /**
  * Created by kreuter on 10/18/15.
  */
+import backEnd.*;
 public class mainClass {
     public static void main(String[] args){
 
---
+++ src/setup/newInstall.java
@@ -0,0 +1,19 @@
+package setup;
+
+/**
+ * Created by kreuter on 10/18/15.
+ */
+import java.io.*;
+public class newInstall {
+
+    private String username;
+    private String password;
+
+    public newInstall(String username, String password){
+        this.password = password;
+        this.username = username;
+    }
+    protected void checkInstall(){
+
+    }
+}

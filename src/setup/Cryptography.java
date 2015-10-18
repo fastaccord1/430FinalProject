c14770c744bdaaadfcc5198b9aa927200db47e4c
Kevin Reuter <fastaccord1@gmail.com>
2015-10-18 13:11:16 -0500

    added decrypt method

--- src/setup/Cryptography.java
+++ src/setup/Cryptography.java
@@ -5,13 +5,41 @@ package setup;
  */
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class Cryptography {
     private static String INITIALKEY = "hrgYKTyUZP7OHhKAv04d";
     private SecretKey secretKey;
+    private Cipher aesCipher;
 
     public Cryptography() {
         byte[] keyBytes = INITIALKEY.getBytes();
         secretKey = new SecretKeySpec(keyBytes, "AES");
+        aesCipher = null;
+        try {
+            aesCipher = Cipher.getInstance("AES");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected String decrypt(String encrypted){
+        String decrypted = null;
+        byte[] encryptedBytes = encrypted.getBytes();
+        try {
+            aesCipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] plainText = aesCipher.doFinal(encryptedBytes);
+            return plainText.toString();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return decrypted;
     }
 }

4170aa6248e7de4ea09010856043c80cf0962cdb
Kevin Reuter <fastaccord1@gmail.com>
2015-10-18 12:24:36 -0500

    adding methods to check install status

--- src/setup/newInstall.java
+++ src/setup/newInstall.java
@@ -5,15 +5,28 @@ package setup;
  */
 import java.io.*;
 public class newInstall {
-
+    private static String INSTALL_FILE = "430.conf";
+    private Boolean installed;
     private String username;
     private String password;
 
     public newInstall(String username, String password){
         this.password = password;
         this.username = username;
+        this.installed = false;
+        checkInstall();
     }
     protected void checkInstall(){
+        File f = new File(System.getProperty("user.home") + "/" + INSTALL_FILE);
+        if(f.exists() && !f.isDirectory()) {
+            createInstall();
+        } else{
+            installed = true;
+            return;
+        }
+    }
 
+    protected void createInstall(){
+        installed = true;
     }
 }

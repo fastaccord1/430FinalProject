593f017a40bbb22d271e97e147f1816292e2be56
Kevin Reuter <fastaccord1@gmail.com>
2015-10-18 12:39:36 -0500

    moved installPath to be class variable

--- src/setup/newInstall.java
+++ src/setup/newInstall.java
@@ -10,28 +10,33 @@ public class newInstall {
     private Boolean installed;
     private String username;
     private String password;
+    private String installPath;
 
     public newInstall(String username, String password){
         this.password = password;
         this.username = username;
         this.installed = false;
         this.osName = System.getProperty("os.name");
+
+        this.installPath = null;
+        String userHome = System.getProperty("user.home");
+        if(osName.equals("Windows")) {
+            this.installPath = userHome + "\\" + INSTALL_FILE;
+        } else if(osName.equals("Linux")){
+            this.installPath = userHome + "/" + INSTALL_FILE;
+        } else{
+            System.out.println("Something didn't go right");
+        }
+
         checkInstall();
     }
     protected void checkInstall(){
         if(installed) return;
-        String installPath = null;
-        String userHome = System.getProperty("user.home");
-        if(osName.equals("Windows")) {
-            installPath = userHome + "\\" + INSTALL_FILE;
-        } else if(osName.equals("Linux")){
-            installPath = userHome + "/" + INSTALL_FILE;
-        } else{
-            System.out.println("Something didn't go right");
-        }
+
         File f = new File(installPath);
         if(f.exists() && !f.isDirectory()) {
             createInstall();
+
         } else{
             installed = true;
             return;
@@ -39,6 +44,7 @@ public class newInstall {
     }
 
     protected void createInstall(){
+
         installed = true;
     }
 }

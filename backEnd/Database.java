c1ddf1b41a7875cfab7c6139af24da4a07f3dd8b
Kevin Reuter <fastaccord1@gmail.com>
2015-10-17 23:19:44 -0500

    added connection method

--- backEnd/Database.java
+++ backEnd/Database.java
@@ -1,14 +1,40 @@
 /**
  * Created by kreuter on 10/18/15.
  */
+import java.sql.*;
+
 package backEnd;
 
 public class Database {
+    private static String DRIVER = "oracle.jdbc.driver.OracleDriver";
     private String url;
     private String username;
+    private String password;
+    Connection conn;
 
-    public Database(){
+    public Database(String url, String username, String password){
+        this.url = url;
+        this.username = username;
+        this.password = password;
 
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        conn = connect();
+
+    }
+
+    public Connection connect(){
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url,username,password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
     }
 
 }

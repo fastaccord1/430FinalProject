dcfce1323f3e0f158e4a0b443d7fdd05d17bfc82
Kevin Reuter <fastaccord1@gmail.com>
2015-10-18 19:39:49 -0500

    Changed name to better fit naming scheme

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,12 +2,15 @@
  * Created by kreuter on 10/18/15.
  */
 import backEnd.*;
+
 import setup.*;
-public class mainClass {
+public class MainClass {
     public static void main(String[] args){
         NewInstall test = new NewInstall("test", "test");
         if(!test.checkInstall()){
             test.createInstall();
         }
+
+
     }
 }

56384b67577ee4af505e0c7e6a1772359234bd05
Kevin Reuter <kreuter@siu.edu>
2015-10-20 13:07:25 -0500

    removed unneeded code.

--- src/MainClass.java
+++ src/MainClass.java
@@ -7,17 +7,7 @@ import setup.*;
 public class MainClass {
     public static void main(String[] args){
         if(!NewInstall.checkInstall()){
-            /*
-            Console console = System.console();
-            console.printf("Please input username: ");
-            String username = console.readLine();
 
-            console.printf("\n");
-
-            console.printf("Please input password: ");
-            char[] passwordChars = console.readPassword();
-            String password = new String(passwordChars);
-            */
             NewInstall install = new NewInstall();
 
             install.createInstall();

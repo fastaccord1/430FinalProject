e1d96a14c5f1342c686d23d6bb3a68b5c71261aa
Kevin Reuter <fastaccord1@gmail.com>
2015-10-21 10:30:10 -0500

    Adding comments

--- src/backEnd/MainClassHolder.java
+++ src/backEnd/MainClassHolder.java
@@ -2,34 +2,49 @@ package backEnd;
 
 import frontEnd.InitialSetup;
 import setup.NewInstall;
-//import setup.PostInstall;
+
 
 /**
  * Created by kreuter on 10/20/15.
  */
 public class MainClassHolder {
+    // Database object to be used for connection
     private static Database database;
+    // NewInstall object to be used for initializing database
     private static NewInstall newInstall;
 
-
+    /**
+     * Default constructor to initialize variables
+     */
     public MainClassHolder(){
         database = null;
         InitialSetup.main(null);
         newInstall = null;
-
-
     }
 
-
+    /**
+     * Method that initiates a check for the database tables
+     */
     public static void checkInstall(){
         newInstall = new NewInstall(database);
-        newInstall.checkInstall();
+        if(!newInstall.checkInstall()){
+
+        }
     }
 
+    /**
+     * Getter for the Database object used for the main connection
+     * @return Database object for the database connection
+     */
     public static Database getDatabase() {
         return database;
     }
 
+    /**
+     * Setter method that creates a new Database object
+     * @param username String username to be used for database
+     * @param password String password to be used for the database
+     */
     public static void setDatabase(String username, String password) {
         MainClassHolder.database = new Database(username, password);
     }

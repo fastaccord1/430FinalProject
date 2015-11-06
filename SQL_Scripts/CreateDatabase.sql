799fa4937444409dce6da0d54c433a3cd6f14b89
Kevin Reuter <fastaccord1@gmail.com>
2015-11-06 10:37:59 -0600

    Added primary keys

--- SQL_Scripts/CreateDatabase.sql
+++ SQL_Scripts/CreateDatabase.sql
@@ -9,20 +9,20 @@ DROP TABLE Department;
 
 /* Create new tables with constraints */
 /* Create Department table */
-CREATE TABLE Department(did INTEGER, dname VARCHAR(50));
+CREATE TABLE Department(did INTEGER, dname VARCHAR(50), PRIMARY KEY(did));
 /* Create Student table */
 CREATE TABLE Student(sid INTEGER, sname VARCHAR(100), major VARCHAR(50), s_level VARCHAR(30), age INTEGER,
-CHECK (s_level = 'Freshman' OR 'Sophomore' OR 'Junior' OR 'Senior' OR 'Masters' OR 'PHD'));
+CHECK (s_level = 'Freshman' OR 'Sophomore' OR 'Junior' OR 'Senior' OR 'Masters' OR 'PHD'), PRIMARY KEY(sid));
 
 /* Create Faculty table */
-CREATE TABLE Faculty(fid INTEGER, fname VARCHAR(100), deptid INTEGER REFERENCES Department(did));
+CREATE TABLE Faculty(fid INTEGER, fname VARCHAR(100), deptid INTEGER REFERENCES Department(did), PRIMARY KEY(fid));
 
 /* Create Staff table */
-CREATE TABLE Staff(sid INTEGER, sname VARCHAR(100), deptid INTEGER REFERENCES Department(did));
+CREATE TABLE Staff(sid INTEGER, sname VARCHAR(100), deptid INTEGER REFERENCES Department(did), PRIMARY KEY(sid));
 
 /* Create Courses table */
 CREATE TABLE Courses(cid VARCHAR(7), cname VARCHAR(50), meets_at VARCHAR(20), room VARCHAR(30), fid INTEGER REFERENCES
-Faculty(fid), limit INTEGER CHECK(limit < 300));
+Faculty(fid), limit INTEGER CHECK(limit < 200), PRIMARY KEY(cid));
 
 /* Create Enrolled table */
 CREATE TABLE Enrolled(sid INTEGER, cid INTEGER, exam1 INTEGER, exam2 INTEGER, final INTEGER, FOREIGN KEY(sid) REFERENCES

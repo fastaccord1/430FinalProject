7dfe21b6b9564dc512dda0469f2886b668cca34c
Kevin Reuter <fastaccord1@gmail.com>
2015-11-06 10:36:33 -0600

    Removed table creation from Java

--- src/cs430/finalProject/setup/NewInstall.java
+++ src/cs430/finalProject/setup/NewInstall.java
@@ -40,7 +40,7 @@ public class NewInstall {
      * creates the tables and then inserts the data.
      */
     public void createInstall(){
-        createTables();
+        //createTables();
         insertDepartmentData();
         insertStudentData(null);
         insertFacultyData();
@@ -55,34 +55,6 @@ public class NewInstall {
      */
     public void setDatabase(Database database){
         this.database = database;
-    }
-
-    /**
-     * This method creates the tables required for running the program
-     */
-    public void createTables(){
-        String createStudent = "CREATE TABLE Student(sid INT, sname VARCHAR(50), major VARCHAR(50), " +
-                "level VARCHAR(50), age INT, PRIMARY KEY(sid))";
-        database.executeInsertUpdate(createStudent);
-
-        String createDepartment = "CREATE TABLE Department(did INT, dname VARCHAR(50), PRIMARY KEY(did))";
-        database.executeInsertUpdate(createDepartment);
-
-        String createStaff = "CREATE TABLE Staff(sid INT, sname VARCHAR(50), deptid INT, PRIMARY KEY(sid), " +
-                "FOREIGN KEY(deptid) REFERENCES Department(did))";
-        database.executeInsertUpdate(createStaff);
-
-        String createFaculty = "CREATE TABLE Faculty(fid INT, fname VARCHAR(50), deptid INT, PRIMARY KEY(fid), " +
-                "FOREIGN KEY(deptid) REFERENCES Department(did))";
-        database.executeInsertUpdate(createFaculty);
-
-        String createCourses = "CREATE TABLE Courses(cid INT, cname VARCHAR(50), meets_at VARCHAR(50), " +
-                "room VARCHAR(50), fid INT REFERENCES Facutly(fid), limit INT, PRIMARY KEY(cid))";
-        database.executeInsertUpdate(createCourses);
-
-        String createEnrolled = "CREATE TABLE Enrolled(sid INT, cid INT, exam1 INT, exam2 INT, final INT, " +
-                "FOREIGN KEY(sid) REFERENCES Student(sid), FOREIGN KEY(cid) REFERENCES Course(cid))";
-        database.executeInsertUpdate(createEnrolled);
     }
 
     /**

b8e62dc691278c2250754af9c4774891e88df60a
Kevin Reuter <fastaccord1@gmail.com>
2015-11-07 14:31:01 -0600

    Finished faculty inserts

--- src/cs430/finalProject/SQL_Scripts/CreateEntries.sql
+++ src/cs430/finalProject/SQL_Scripts/CreateEntries.sql
@@ -9,15 +9,36 @@ INSERT INTO Department VALUES (470741, 'Information Systems Technology');
 INSERT INTO Department VALUES (477363, 'Business Administration');
 INSERT INTO Department VALUES (416684, 'Medicine');
 
+INSERT INTO Faculty VALUES (221320, 'Carolyn Murphy', 416684);
+INSERT INTO Faculty VALUES (269040, 'Thomas Carpenter', 423231);
+INSERT INTO Faculty VALUES (214074, 'Lisa Bowman', 479680);
+INSERT INTO Faculty VALUES (217222, 'Albert Watson', 449959);
+INSERT INTO Faculty VALUES (268862, 'Janice Gutierrez', 426304);
+INSERT INTO Faculty VALUES (283132, 'Louis Pierce', 407709);
+INSERT INTO Faculty VALUES (299657, 'Doris Gibson', 470741);
+INSERT INTO Faculty VALUES (271928, 'Denise Patterson', 477363);
+INSERT INTO Faculty VALUES (292016, 'Janice Cunningham', 416684);
+INSERT INTO Faculty VALUES (215748, 'Emily Ward', 435339);
 
-INSERT INTO Student VALUES(101, 'Kimberleigh Vernon', 'Computer Science', 'Junior', 21);
-INSERT INTO Student VALUES(102, 'Fern Aukes', 'Electrical Engineering', 'Sophomore', 20);
-INSERT INTO Student VALUES(103, 'Gerlinde Ravenna', 'English', 'Freshman', 18);
-INSERT INTO Student VALUES(104, 'Lois P. Huff', 'Philosophy', 'Masters', 24);
-INSERT INTO Student VALUES(105, 'Lorraine J. Bailey', 'Computer Science', 'Senior', 22);
-INSERT INTO Student VALUES(106, 'Anthony B. Lucero', 'English', 'PHD', 26);
-INSERT INTO Student VALUES(107, 'Donna A. Wegman', 'Information Systems Technology', 'Freshman', 18);
-INSERT INTO Student VALUES(108, 'Margaret J. Bishop', 'Business Administration', 'Masters', 24);
-INSERT INTO Student VALUES(109, 'Vivian D. Shive', 'Computer Science', 'PHD', 25);
-INSERT INTO Student VALUES(110, 'Joann O. Cormack', 'Electrical Engineering', 'Junior', 30);
+INSERT INTO Staff VALUES (350089);
+INSERT INTO Staff VALUES (330543);
+INSERT INTO Staff VALUES (355611);
+INSERT INTO Staff VALUES (351317);
+INSERT INTO Staff VALUES (334916);
+INSERT INTO Staff VALUES (379154);
+INSERT INTO Staff VALUES (338500);
+INSERT INTO Staff VALUES (369356);
+INSERT INTO Staff VALUES (346411);
+INSERT INTO Staff VALUES (378544);
+
+INSERT INTO Student VALUES(119731, 'Kimberleigh Vernon', 'Computer Science', 'Junior', 21);
+INSERT INTO Student VALUES(139386, 'Fern Aukes', 'Electrical Engineering', 'Sophomore', 20);
+INSERT INTO Student VALUES(159995, 'Gerlinde Ravenna', 'English', 'Freshman', 18);
+INSERT INTO Student VALUES(117335, 'Lois P. Huff', 'Philosophy', 'Masters', 24);
+INSERT INTO Student VALUES(123932, 'Lorraine J. Bailey', 'Computer Science', 'Senior', 22);
+INSERT INTO Student VALUES(192090, 'Anthony B. Lucero', 'English', 'PHD', 26);
+INSERT INTO Student VALUES(115982, 'Donna A. Wegman', 'Information Systems Technology', 'Freshman', 18);
+INSERT INTO Student VALUES(173541, 'Margaret J. Bishop', 'Business Administration', 'Masters', 24);
+INSERT INTO Student VALUES(169810, 'Vivian D. Shive', 'Computer Science', 'PHD', 25);
+INSERT INTO Student VALUES(130412, 'Joann O. Cormack', 'Electrical Engineering', 'Junior', 30);
 

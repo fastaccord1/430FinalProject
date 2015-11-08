090ab10b932c8035572fc3b34ca711ed05437f30
Kevin Reuter <fastaccord1@gmail.com>
2015-11-08 16:06:27 -0600

    Optimising code in Runner

--- src/cs430/finalProject/Runner.java
+++ src/cs430/finalProject/Runner.java
@@ -1,30 +1,23 @@
 package cs430.finalProject; /**
  * 430FinalProject
  * Created by kreuter on 10/18/15.
+ *
  * @author Kevin Reuter
  */
+
 import cs430.finalProject.backEnd.MainClassHolder;
-import cs430.finalProject.frontEnd.StaffForm;
 
 public class Runner {
 
     /**
      * This method only starts the program by calling MainClassHolder
+     *
      * @param args String array of arguments
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         MainClassHolder mainClass = new MainClassHolder();
 
-        if(args.length == 1){
-            if(args[0].equals("--fresh")){
-                //System.out.println("We made it!");
-                //mainClass.freshInstall();
-            } else{
-                System.exit(1);
-            }
-        } else{
-            mainClass.noInstall();
-        }
-        //mainClass.close();
+        mainClass.noInstall();
+
     }
 }

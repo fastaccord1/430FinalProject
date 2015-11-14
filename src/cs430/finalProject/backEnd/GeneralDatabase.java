2d8a90e716318f03ad9aa6a903acfe30ee8a2520
Kevin Reuter <fastaccord1@gmail.com>
2015-11-14 12:46:23 -0600

    Adding search methods for Department

--- src/cs430/finalProject/backEnd/GeneralDatabase.java
+++ src/cs430/finalProject/backEnd/GeneralDatabase.java
@@ -3,12 +3,24 @@ package cs430.finalProject.backEnd;
 import oracle.jdbc.driver.OracleDriver;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
+ * This class handles database operations not related to Faculty, Staff or Students
  * Created by kreuter on 11/13/15.
+ *
+ * @author Kevin Reuter
  */
 public class GeneralDatabase extends Database {
+
+    /**
+     * Constructor for GeneralDatabase class
+     *
+     * @param username Username for the database
+     * @param password Password for the database
+     */
     public GeneralDatabase(String username, String password) {
         super();
         try {
@@ -17,5 +29,49 @@ public class GeneralDatabase extends Database {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public Object[][] searchDepartment() {
+        String query = "SELECT * FROM Department";
+        Object[][] output;
+        try {
+            int count = getCount(query);
+            output = new Object[count][2];
+            ResultSet rs = executeQuery(query);
+            for (int i = 0; rs.next(); i++) {
+                output[i][0] = rs.getInt("did");
+                output[i][1] = rs.getString("dname");
+            }
+            return output;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Object[][] searchDepartment(int did, String dName) {
+        String query = "SELECT * FROM Department WHERE";
+        ArrayList<String> conditions = new ArrayList<>();
+        if (did != -1) {
+            conditions.add(" did = " + did);
+        }
+        if (dName != null) {
+            conditions.add(" dname = " + dName);
+        }
+
+        try {
+            query = finishQuery(query, conditions);
+            int count = getCount(query);
+            Object[][] output = new Object[count][2];
+            ResultSet rs = executeQuery(query);
+            for (int i = 0; rs.next(); i++) {
+                output[i][0] = rs.getInt("did");
+                output[i][1] = rs.getString("dname");
+            }
+            return output;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }

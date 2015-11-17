0967bd1a07b4ad9c4a086f75d7ea2f9986a9a5c8
Kevin Reuter <fastaccord1@gmail.com>
2015-11-16 20:22:45 -0600

    Changed editable arrays

--- src/cs430/finalProject/frontEnd/StaffForm.java
+++ src/cs430/finalProject/frontEnd/StaffForm.java
@@ -2200,7 +2200,7 @@ public class StaffForm extends javax.swing.JFrame {
                 java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false
+                false, false, false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -2483,7 +2483,7 @@ public class StaffForm extends javax.swing.JFrame {
             courseColumns
         ) {
             boolean[] canEdit = new boolean [] {
-                true, false, false, false, false
+                true, false, false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
@@ -2649,7 +2649,7 @@ public class StaffForm extends javax.swing.JFrame {
             courseColumns
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, true, true
+                false, false, false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {

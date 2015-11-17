cfae26024a06868b91d4afa7b5d38059ae66abf3
Kevin Reuter <fastaccord1@gmail.com>
2015-11-16 20:30:43 -0600

    Removed column classes

--- src/cs430/finalProject/frontEnd/StaffForm.java
+++ src/cs430/finalProject/frontEnd/StaffForm.java
@@ -2218,16 +2218,10 @@ public class StaffForm extends javax.swing.JFrame {
             generalDatabase.searchCourse(),
             courseColumns
         ) {
-            Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class
-            };
+
             boolean[] canEdit = new boolean [] {
                 false, false, false, false, false, false
             };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];

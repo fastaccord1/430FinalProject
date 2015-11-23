bf71ce5b1fde447ce534fbe139444da5c6605344
Kevin Reuter <fastaccord1@gmail.com>
2015-11-23 10:43:53 -0600

    Added not null where necessary and auto-formatted code

--- SQL_Scripts/CreateDatabase.sql
+++ SQL_Scripts/CreateDatabase.sql
@@ -13,16 +13,27 @@ DROP VIEW courseView;
 
 /* Create new tables with constraints */
 /* Create Department table */
-CREATE TABLE Department(did INTEGER, dname VARCHAR(50), PRIMARY KEY(did));
+CREATE TABLE Department (
+  did   INTEGER,
+  dname VARCHAR(50),
+  PRIMARY KEY (did)
+);
 /* Create Student table */
-CREATE TABLE Student(sid INTEGER, sname VARCHAR(100), major VARCHAR(50), s_level VARCHAR(30), age INTEGER,
-PRIMARY KEY(sid), CHECK (s_level = 'Freshman' OR s_level = 'Sophomore' OR s_level = 'Junior' OR
-                         s_level = 'Senior' OR s_level = 'Masters' OR s_level = 'PhD'));
+CREATE TABLE Student (
+  sid     INTEGER,
+  sname   VARCHAR(100) NOT NULL,
+  major   VARCHAR(50)  NOT NULL,
+  s_level VARCHAR(30)  NOT NULL,
+  age     INTEGER      NOT NULL,
+  PRIMARY KEY (sid),
+  CHECK (s_level = 'Freshman' OR s_level = 'Sophomore' OR s_level = 'Junior' OR
+         s_level = 'Senior' OR s_level = 'Masters' OR s_level = 'PhD')
+);
 
 /* Create Faculty table */
 CREATE TABLE Faculty (
   fid    INTEGER,
-  fname  VARCHAR(100),
+  fname  VARCHAR(100) NOT NULL,
   deptid INTEGER,
   PRIMARY KEY (fid),
   FOREIGN KEY (deptid) REFERENCES
@@ -32,7 +43,7 @@ CREATE TABLE Faculty (
 /* Create Staff table */
 CREATE TABLE Staff (
   sid    INTEGER,
-  sname  VARCHAR(100),
+  sname  VARCHAR(100) NOT NULL,
   deptid INTEGER,
   PRIMARY KEY (sid),
   FOREIGN KEY (deptid) REFERENCES
@@ -40,15 +51,26 @@ CREATE TABLE Staff (
 );
 
 /* Create Courses table */
-CREATE TABLE Courses(cid   VARCHAR(10), cname VARCHAR(100), meets_at VARCHAR(20), room VARCHAR(30), fid INTEGER REFERENCES
-  Faculty (fid) ON DELETE CASCADE,
-                     limit INTEGER CHECK (limit < 200),
+CREATE TABLE Courses (
+  cid      VARCHAR(10),
+  cname    VARCHAR(100) NOT NULL,
+  meets_at VARCHAR(20),
+  room     VARCHAR(30),
+  fid      INTEGER REFERENCES
+    Faculty (fid) ON DELETE CASCADE,
+  limit    INTEGER CHECK (limit < 200),
   PRIMARY KEY (cid)
 );
 
 /* Create Enrolled table */
-CREATE TABLE Enrolled(sid INTEGER, cid VARCHAR(10), exam1 INTEGER, exam2 INTEGER, final INTEGER, FOREIGN KEY(sid) REFERENCES
-  Student (sid) ON DELETE CASCADE,
+CREATE TABLE Enrolled (
+  sid   INTEGER,
+  cid   VARCHAR(10),
+  exam1 INTEGER,
+  exam2 INTEGER,
+  final INTEGER,
+  FOREIGN KEY (sid) REFERENCES
+    Student (sid) ON DELETE CASCADE,
   FOREIGN KEY (cid) REFERENCES Courses (cid) ON DELETE CASCADE
 );
 

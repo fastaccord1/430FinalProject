67b3caabe1c6723ad6a3b6072503d9c5e9e1e3f1
Kevin Reuter <fastaccord1@gmail.com>
2015-11-23 18:33:58 -0600

    Adding comments to GeneralDatabase

--- src/cs430/finalProject/backEnd/GeneralDatabase.java
+++ src/cs430/finalProject/backEnd/GeneralDatabase.java
@@ -79,6 +79,11 @@ public class GeneralDatabase extends Database {
         return null;
     }
 
+    /**
+     * Gets all department names from the database
+     *
+     * @return String array of database names
+     */
     public String[] getDepartmentNames() {
         String query = "SELECT dname FROM Department";
         try {
@@ -95,6 +100,12 @@ public class GeneralDatabase extends Database {
         return null;
     }
 
+    /**
+     * Gets the department's ID from its name
+     *
+     * @param deptName The name of the department to be found
+     * @return The department's ID
+     */
     public int getDepartmentId(String deptName) {
         String query = "SELECT did FROM Department WHERE dname = '" + deptName + "'";
         ResultSet rs;
@@ -110,6 +121,12 @@ public class GeneralDatabase extends Database {
         return output;
     }
 
+    /**
+     * Inserts a department into the database
+     *
+     * @param id   The ID for the new department
+     * @param name The name for the new department
+     */
     public void insertDepartment(int id, String name) {
         String statement = "INSERT INTO Department VALUES(";
         statement += id + ", ";
@@ -123,6 +140,13 @@ public class GeneralDatabase extends Database {
         }
     }
 
+    /**
+     * Updates a department in the database
+     *
+     * @param oldId The old ID for the department
+     * @param id    The new ID for the department
+     * @param name  The new name for the department
+     */
     public void updateDepartment(int oldId, int id, String name) {
         String statement = "UPDATE Department SET ";
         statement += "did = " + id + ", ";
@@ -136,6 +160,11 @@ public class GeneralDatabase extends Database {
         }
     }
 
+    /**
+     * Gets all courses from the database
+     *
+     * @return Two-dimensional array of courses
+     */
     public Object[][] searchCourse() {
         String query = "SELECT * FROM CourseView";
         try {
@@ -148,6 +177,17 @@ public class GeneralDatabase extends Database {
         return null;
     }
 
+    /**
+     * Searches database for specific courses
+     *
+     * @param id    The ID to be found
+     * @param name  The course name to be found
+     * @param meets When the course meets
+     * @param room  The room where the course meets
+     * @param fname The faculty name for the course
+     * @param limit The limit for the course
+     * @return Two-dimensional array of courses
+     */
     public Object[][] searchCourse(String id, String name, String meets, String room, String fname, int limit) {
         String query = "SELECT * FROM courseView WHERE";
         ArrayList<String> conditions = new ArrayList<>();
@@ -179,6 +219,14 @@ public class GeneralDatabase extends Database {
         return null;
     }
 
+    /**
+     * Converts results of course search to array for table
+     *
+     * @param rs    The results to be converted
+     * @param count The count of results
+     * @return Two-dimensional array for the table
+     * @throws SQLException
+     */
     public Object[][] getCourseResults(ResultSet rs, int count) throws SQLException {
         Object[][] output = new Object[count][6];
         for (int i = 0; rs.next(); i++) {
@@ -192,6 +240,16 @@ public class GeneralDatabase extends Database {
         return output;
     }
 
+    /**
+     * Inserts a course into the database
+     *
+     * @param id    The ID for the course
+     * @param name  The name for the course
+     * @param meets When the course meets
+     * @param room  The room where the course meets
+     * @param fId   The faculty ID for the course
+     * @param limit The limit for the course
+     */
     public void insertCourse(String id, String name, String meets, String room, int fId, int limit) {
         String statement = "INSERT INTO Courses VALUES(";
         statement += "'" + id + "', ";
@@ -208,6 +266,17 @@ public class GeneralDatabase extends Database {
         }
     }
 
+    /**
+     * Updates a course in the database
+     *
+     * @param oldCid The old ID for the course
+     * @param cid    The new ID for the course
+     * @param name   The new name for the course
+     * @param meets  The new time at which the course meets
+     * @param room   The new room where the course meets
+     * @param fid    The new faculty ID for the course
+     * @param limit  The new limit for the course
+     */
     public void updateCourse(String oldCid, String cid, String name, String meets, String room, int fid, int limit) {
         String statement = "UPDATE Courses SET";
         statement += " cid = '" + cid + "', ";
@@ -223,6 +292,11 @@ public class GeneralDatabase extends Database {
         }
     }
 
+    /**
+     * Gets all course IDs from the database
+     *
+     * @return String array of all course IDs
+     */
     public String[] getCourses() {
         String query = "SELECT cid FROM Courses";
         try {
@@ -239,6 +313,11 @@ public class GeneralDatabase extends Database {
         return null;
     }
 
+    /**
+     * Gets all results from the Enrolled table
+     *
+     * @return Two-dimensional array of enrolled students
+     */
     public Object[][] searchEnrolled() {
         String query = "SELECT * FROM enrolledStudent";
         try {
@@ -249,6 +328,16 @@ public class GeneralDatabase extends Database {
         return null;
     }
 
+    /**
+     * Searches database for specific enrolled students
+     *
+     * @param cid       The ID to be found
+     * @param sName     The student to be found
+     * @param exam1     The exam 1 score to be found
+     * @param exam2     The exam 2 score to be found
+     * @param finalExam The final exam score to be found
+     * @return A two-dimensional array for the table
+     */
     public Object[][] searchEnrolled(String cid, String sName, int exam1, int exam2, int finalExam) {
         String query = "SELECT * FROM enrolledStudent WHERE";
         ArrayList<String> conditions = new ArrayList<>();
@@ -278,6 +367,12 @@ public class GeneralDatabase extends Database {
         return null;
     }
 
+    /**
+     * Searches for all entries in Enrolled for courses taught by a specific faculty
+     *
+     * @param fid The faculty's ID
+     * @return Two-dimensional array for the table
+     */
     public Object[][] searchEnrolled(int fid) {
         String query = "SELECT * FROM enrolledStudent WHERE fid = " + fid;
         try {
@@ -288,6 +383,16 @@ public class GeneralDatabase extends Database {
         return null;
     }
 
+    /**
+     * Searches for specific entry in Enrolled for courses taught by a specific faculty
+     *
+     * @param fid       The faculty's ID
+     * @param sName     Student's name to be searched
+     * @param exam1     Exam 1 score to be found
+     * @param exam2     Exam 2 score to be found
+     * @param finalExam Final exam score to be found
+     * @return Two-dimensional array for the table
+     */
     public Object[][] searchEnrolled(int fid, String sName, int exam1, int exam2, int finalExam) {
         String query = "SELECT * FROM enrolledStudent WHERE fid = " + fid + " AND";
         ArrayList<String> conditions = new ArrayList<>();
@@ -313,6 +418,15 @@ public class GeneralDatabase extends Database {
         return null;
     }
 
+    /**
+     * Inserts new entry into Enrolled table
+     *
+     * @param cid       The course ID
+     * @param sid       The student's ID
+     * @param exam1     The exam 1 score
+     * @param exam2     The exam 2 score
+     * @param finalExam The final exam score
+     */
     public void insertEnrolled(String cid, int sid, int exam1, int exam2, int finalExam) {
         String statement = "INSERT INTO Enrolled VALUES(";
         statement += sid + ", ";
@@ -328,6 +442,17 @@ public class GeneralDatabase extends Database {
         }
     }
 
+    /**
+     * Updates an entry in the Enrolled table
+     *
+     * @param oldCid    The old course ID
+     * @param oldSid    The old student ID
+     * @param cid       The new course ID
+     * @param sid       The new student ID
+     * @param exam1     The new exam 1 score
+     * @param exam2     The new exam 2 score
+     * @param finalExam The new final exam score
+     */
     public void updateEnrolled(String oldCid, int oldSid, String cid, int sid, int exam1, int exam2, int finalExam) {
         String statement = "UPDATE Enrolled SET";
         statement += " cid = '" + cid + "', ";
@@ -344,6 +469,11 @@ public class GeneralDatabase extends Database {
         }
     }
 
+    /**
+     * Deletes a course from the database
+     *
+     * @param id The course ID to be deleted
+     */
     public void deleteCourse(String id) {
         String statement = "DELETE FROM Courses WHERE cid = '" + id + "'";
         try {
@@ -353,6 +483,11 @@ public class GeneralDatabase extends Database {
         }
     }
 
+    /**
+     * Deletes a department from the database
+     *
+     * @param id The department ID to be deleted
+     */
     public void deleteDepartment(int id) {
         String statement = "DELETE FROM Department WHERE did = " + id;
         try {
@@ -362,6 +497,12 @@ public class GeneralDatabase extends Database {
         }
     }
 
+    /**
+     * Deletes an enrolled student from the database
+     *
+     * @param cid The course ID
+     * @param sid The student ID
+     */
     public void deleteEnrolled(String cid, int sid) {
         String statement = "DELETE FROM Enrolled WHERE sid = " + sid + " AND cid = '" + cid + "'";
         try {
@@ -371,6 +512,12 @@ public class GeneralDatabase extends Database {
         }
     }
 
+    /**
+     * Gets array of course info for specific course
+     *
+     * @param cid The ID to be found
+     * @return Object array of course info
+     */
     public Object[] getCourseInfo(String cid) {
         String query = "SELECT * FROM courseView WHERE cid = '" + cid + "'";
         Object[] data = new Object[5];

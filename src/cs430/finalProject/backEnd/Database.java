123ac7a3d4e93928e52ea5f74cf308ed51db32ea
Kevin Reuter <fastaccord1@gmail.com>
2015-11-23 18:13:52 -0600

    Adding comments to Database

--- src/cs430/finalProject/backEnd/Database.java
+++ src/cs430/finalProject/backEnd/Database.java
@@ -37,57 +37,6 @@ public class Database {
         }
     }
 
-    /**
-     * Finds the type of a certain id
-     *
-     * @param id The id to be checked against the database
-     * @return An int representing the type of employee. -1 if not found
-     * @throws SQLException
-     */
-    public int findType(int id) throws SQLException {
-        int output = -1;
-        String queryStudent = "SELECT * FROM Student WHERE sid = " + id;
-        String queryStaff = "SELECT * FROM Staff WHERE sid = " + id;
-        String queryFaculty = "SELECT * FROM Faculty WHERE fid = " + id;
-        if (searchDB(queryStudent)) {
-            return 1;
-        } else if (searchDB(queryStaff)) {
-            return 2;
-        } else if (searchDB(queryFaculty)) {
-            return 3;
-        }
-        return output;
-    }
-
-    /**
-     * Searches database for query and returns true if found
-     *
-     * @param query The query to be run against the database
-     * @return Returns true if the query returned a result. False if query didn't return a result.
-     */
-    private boolean searchDB(String query) {
-        Statement statement = null;
-        try {
-            statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery(query);
-            if (rs.next()) {
-                rs.close();
-                statement.close();
-                return true;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                assert statement != null;
-                statement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return false;
-    }
-
 
     /**
      * Finishes the query from the conditions in the ArrayList
@@ -175,6 +124,14 @@ public class Database {
         statement.execute(query);
     }
 
+    /**
+     * Separates results from query into array for table
+     *
+     * @param query       The query to be run
+     * @param columnNames The column names to be grabbed
+     * @return A two-dimensional array for the table
+     * @throws SQLException
+     */
     protected Object[][] getStaffFacultyResults(String query, String[] columnNames) throws SQLException {
         Object[][] output;
         int count = getCount(query);
@@ -188,6 +145,13 @@ public class Database {
         return output;
     }
 
+    /**
+     * Separates student results from query into array for table
+     *
+     * @param query The query to be run
+     * @return A two-dimensional array for the table
+     * @throws SQLException
+     */
     protected Object[][] getStudentResults(String query) throws SQLException {
         Object[][] output = new Object[getCount(query)][5];
         ResultSet rs = executeQuery(query);
@@ -201,6 +165,13 @@ public class Database {
         return output;
     }
 
+    /**
+     * Separates enrolled student results from query into array for table
+     *
+     * @param query The query to be run
+     * @return A two-dimensional array for the table
+     * @throws SQLException
+     */
     protected Object[][] getStudentEnrolledResults(String query) throws SQLException {
         Object[][] output = new Object[getCount(query)][5];
         ResultSet rs = executeQuery(query);
@@ -214,6 +185,13 @@ public class Database {
         return output;
     }
 
+    /**
+     * Separates enrolled results for faculty into array for a table
+     *
+     * @param query The query to be run
+     * @return A two-dimensional array for the table
+     * @throws SQLException
+     */
     protected Object[][] getFacultyEnrolledResults(String query) throws SQLException {
         Object[][] output = new Object[getCount(query)][5];
         ResultSet rs = executeQuery(query);
